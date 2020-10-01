d9b07ad34043337e42a84dd22f899f339c5927f8
nisha3003 <51108048+nisha3003@users.noreply.github.com>
2020-10-01 17:22:11 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -8,16 +8,12 @@ import Portfolio from "./components/Portfolio";
 import Contacts from "./components/Contacts";
 
 
-function App() {
-  return (
-    <>
-    <CssBaseline/>
-      <Route exact path="/" component={Home}/>
-      <Route path="/resume" component={Resume}/>
-      <Route path="/portfolio" component={Portfolio}/>
-      <Route path="/contacts" component={Contacts}/>
-    </>
-  );
-}
+// Paste here ------
+
+
+
+
+
+// -------------
 
 export default App;
